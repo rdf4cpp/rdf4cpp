3bd169d39fc8f3629ef9305af7f5e80fd99a5999
Alexander Bigerl <alexander@bigerl.eu>
2021-07-22 15:30:20 +0200

    moved examples to a subfolder

---
+++ examples/graphs_and_datasets.cpp
@@ -0,0 +1,48 @@
+#include <rdf4cpp/rdf.hpp>
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    using namespace rdf4cpp::rdf;
+    Graph g;
+
+    Dataset dataset = g.dataset();
+    Dataset dataset2(storage::node::NodeStorage::new_instance());  // Dataset with an independent NodeStorage
+    std::cout << "dataset node storage id: " << dataset.backend()->node_storage().id().value << std::endl;
+    std::cout << "dataset2 node storage id: " << dataset2.backend()->node_storage().id().value << std::endl;
+
+    g.add({IRI{"http://example.com"}, IRI{"http://example.com"}, Literal("text", "en")});
+    g.add({IRI{"http://example.com"}, IRI{"http://example.com"}, Literal("text", "fr")});
+    g.add({IRI{"http://example.com"}, IRI{"http://example.com"}, Literal("txt")});
+    g.add({IRI{"http://example.com"}, IRI{"http://example.com"}, Literal("text")});
+
+    dataset.add({IRI{"http://named_graph.com"}, IRI{"http://example.com"}, IRI{"http://example.com"}, Literal("text")});
+
+    std::cout << "dataset string: \n"
+              << writer::NQuadsWriter(g.dataset()) << std::endl;
+    query::TriplePattern triple_pattern{query::Variable("x"), IRI{"http://example.com"}, query::Variable{"z"}};
+    std::cout << triple_pattern.subject() << std::endl;
+    std::cout << triple_pattern.predicate() << std::endl;
+    std::cout << triple_pattern.object() << std::endl;
+    query::SolutionSequence solutions = g.match(triple_pattern);
+
+    std::cout << "g size " << g.size() << std::endl;
+
+    for (const auto &solution : solutions) {
+        std::cout << "bound variables: " << solution.bound_count() << std::endl;
+
+        for (size_t i = 0; i < solution.bound_count(); ++i) {
+            std::cout << solution.variable(i) << " -> " << solution[i] << std::endl;
+        }
+    }
+
+    if (argc > 1) {
+        std::string ttl_file = argv[1];
+        Dataset ds2;
+        ds2.add_ttl_file(ttl_file);
+        std::cout << "ds2 from " << ttl_file << ":" << std::endl;
+        std::cout << writer::NQuadsWriter(ds2) << std::endl;
+    } else {
+        std::cout << "no test file provided." << std::endl;
+    }
+}
---
+++ examples/literal_datatypes.cpp
@@ -0,0 +1,25 @@
+#include <rdf4cpp/rdf.hpp>
+
+
+#include <iostream>
+
+int main() {
+    using namespace rdf4cpp::rdf;
+
+    Literal float_1_1("1.1", IRI{"http://www.w3.org/2001/XMLSchema#float"});
+
+    std::cout << float_1_1 << std::endl;
+    std::any any_float_ = float_1_1.value();
+    std::cout << any_cast<float>(any_float_) << std::endl;
+    auto float_ = float_1_1.value<float>();  // we know the type at compile time
+    [[maybe_unused]] auto int_ = float_1_1.value<int>();
+    std::cout << float_ << std::endl;
+
+    // update value
+    float_ *= any_cast<datatypes::xsd::Float>(any_float_) * 3;
+    // datatypes::xsd::Float is an alias for the built-in type float
+    std::cout << float_ << std::endl;
+    // make a new literal with new value
+    Literal updated_float = Literal::make(float_);
+    std::cout << updated_float << std::endl;
+}
