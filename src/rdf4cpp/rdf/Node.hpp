b560c7220152bec93392d0175ed15fbe6146f775
nkaralis <nkaralis@mail.uni-paderborn.de>
2021-11-30 14:25:29 +0100

    equality operations for Node

--- src/rdf4cpp/rdf/Node.hpp
+++ src/rdf4cpp/rdf/Node.hpp
@@ -83,6 +83,16 @@ public:
 
     bool operator==(const Node &other) const;
 
+    friend bool operator==(const Node &lhs, const std::unique_ptr<Node> &rhs) noexcept;
+
+    friend bool operator==(const std::unique_ptr<Node> &lhs, Node const &rhs) noexcept;
+
+    friend bool operator==(const std::unique_ptr<Node> &lhs, const std::unique_ptr<Node> &rhs) noexcept;
+
+    friend bool operator==(const Node *lhs, const std::unique_ptr<Node> &rhs) noexcept;
+
+    friend bool operator==(const std::unique_ptr<Node> &lhs, const Node *rhs) noexcept;
+
     std::strong_ordering operator<=>(const Node &other) const;
 
     /**
