4d3a8a5aa0c46030b140530d833e10b7c59f4774
Alexander Bigerl <alexander@bigerl.eu>
2022-06-06 13:52:48 +0200

    standard cmake installing and exporting

---
+++ cmake/install_library.cmake
@@ -0,0 +1,46 @@
+include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+
+function(install_cpp_library TARGET_NAME INCLUDE_PATH)
+    if(NOT ${ARGC} EQUAL 2)
+        message(
+                FATAL_ERROR
+                "you did not specify the target and the include path in the parameter!")
+    endif()
+
+    target_include_directories(
+            ${TARGET_NAME} PUBLIC $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+
+    install(TARGETS ${TARGET_NAME}
+            EXPORT ${TARGET_NAME}-targets
+            ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+            INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+            )
+
+    write_basic_package_version_file("${TARGET_NAME}-config-version.cmake"
+            VERSION ${PROJECT_VERSION}
+            COMPATIBILITY SameMajorVersion)
+
+    configure_package_config_file(
+            "${PROJECT_SOURCE_DIR}/cmake/lib-config.cmake.in"
+            "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}-config.cmake"
+            INSTALL_DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/${PROJECT_NAME}/cmake)
+    # here we have two possibilities: either CMAKE_INSTALL_DATAROOTDIR (share) or CMAKE_INSTALL_LIBDIR (lib/lib64)
+    # we just have to be consistent for one target
+
+    install(
+            EXPORT ${PROJECT_NAME}-targets
+            FILE ${PROJECT_NAME}-targets.cmake
+            NAMESPACE ${PROJECT_NAME}::
+            DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/${PROJECT_NAME}/cmake)
+
+    install(FILES "${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config.cmake"
+            "${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config-version.cmake"
+            DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/${PROJECT_NAME}/cmake)
+
+    install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${INCLUDE_PATH}
+            DESTINATION include
+            FILES_MATCHING PATTERN "*.hpp")
+endfunction()
