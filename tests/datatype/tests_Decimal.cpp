932de935ccbc99f0f209ee45e876e3d217167243
kaimal11 <anuskaimal@gmail.com>
2022-06-08 16:38:24 +0200

    Changes implemented for PR

--- tests/datatype/tests_Decimal.cpp
+++ tests/datatype/tests_Decimal.cpp
@@ -42,40 +42,47 @@ TEST_CASE("Datatype Decimal") {
     CHECK(lit5.value<datatypes::xsd::Decimal>() == value);
 
     value = 1;
-    auto lit6 = Literal{std::to_string(value), type_iri};
+    auto lit6 = Literal{"1", type_iri};
     CHECK(lit6.value<datatypes::xsd::Decimal>() == value);
 
-    value = std::numeric_limits<double>::max();
-    auto lit7 = Literal{std::to_string(value), type_iri};
+    auto lit7 = Literal{"1.0", type_iri};
     CHECK(lit7.value<datatypes::xsd::Decimal>() == value);
+
+    auto lit8 = Literal{"1.00", type_iri};
+    CHECK(lit8.value<datatypes::xsd::Decimal>() == value);
+
+    value = std::numeric_limits<double>::max();
+    auto lit9 = Literal{std::to_string(value), type_iri};
+    CHECK(lit9.value<datatypes::xsd::Decimal>() == value);
 
     CHECK(lit1 != lit2);
     CHECK(lit2 != lit3);
     CHECK(lit1 == lit4);
     CHECK(lit2 == lit5);
-    CHECK(lit1 == lit6);
-    CHECK(lit4 == lit4);
+    CHECK(lit6.value<datatypes::xsd::Decimal>() == lit7.value<datatypes::xsd::Decimal>()); //literal representation will not be matched but the values will match.
+    CHECK(lit6.value<datatypes::xsd::Decimal>() == lit8.value<datatypes::xsd::Decimal>()); //literal representation will not be matched but the values will match.
+    CHECK(lit7.value<datatypes::xsd::Decimal>() == lit7.value<datatypes::xsd::Decimal>()); //literal representation will not be matched but the values will match.
 
     // suppress warnings regarding attribute ‘nodiscard’
     std::any no_discard_dummy = false;
 
-    auto lit8 = Literal{"NAN", type_iri};
-    CHECK_THROWS_WITH_AS(no_discard_dummy = lit8.value(), "XSD Parsing Error", std::runtime_error);
-
-    auto lit9 = Literal{"INF", type_iri};
-    CHECK_THROWS_WITH_AS(no_discard_dummy = lit9.value(), "XSD Parsing Error", std::runtime_error);
-
-    value = INFINITY;
-    auto lit10 = Literal{std::to_string(value), type_iri};
+    auto lit10 = Literal{"NAN", type_iri};
     CHECK_THROWS_WITH_AS(no_discard_dummy = lit10.value(), "XSD Parsing Error", std::runtime_error);
 
-    value = NAN;
-    auto lit11 = Literal{std::to_string(value), type_iri};
+    auto lit11 = Literal{"INF", type_iri};
     CHECK_THROWS_WITH_AS(no_discard_dummy = lit11.value(), "XSD Parsing Error", std::runtime_error);
 
-    auto lit12 = Literal{"454sdsd", type_iri};
+    value = INFINITY;
+    auto lit12 = Literal{std::to_string(value), type_iri};
     CHECK_THROWS_WITH_AS(no_discard_dummy = lit12.value(), "XSD Parsing Error", std::runtime_error);
 
-    auto lit13 = Literal{"2.225E-307", type_iri};
+    value = NAN;
+    auto lit13 = Literal{std::to_string(value), type_iri};
     CHECK_THROWS_WITH_AS(no_discard_dummy = lit13.value(), "XSD Parsing Error", std::runtime_error);
+
+    auto lit14 = Literal{"454sdsd", type_iri};
+    CHECK_THROWS_WITH_AS(no_discard_dummy = lit14.value(), "XSD Parsing Error", std::runtime_error);
+
+    auto lit15 = Literal{"2.225E-307", type_iri};
+    CHECK_THROWS_WITH_AS(no_discard_dummy = lit15.value(), "XSD Parsing Error", std::runtime_error);
 }
