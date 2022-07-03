128c7d3735d098396e66f2b940d5a9724ac9ab38
Alexander Bigerl <alexander@bigerl.eu>
2022-07-03 19:18:53 +0200

    Full conan integration (#39)
    - uses `serd/0.30.12` from conan center if conan is installed and available
    - fixes selection between cmake package and systemlib serd

--- cmake/boilerplate_init.cmake
+++ cmake/boilerplate_init.cmake
@@ -18,5 +18,12 @@ macro(boilerplate_init)
         set(CMAKE_VISIBILITY_INLINES_HIDDEN NO)
     endif ()
 
+    # conan requires cmake build type to be specified and it is generally a good idea
+    if (NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/CMakeCache.txt)
+        if (NOT CMAKE_BUILD_TYPE)
+            set(CMAKE_BUILD_TYPE "Release" CACHE STRING "" FORCE)
+        endif ()
+    endif ()
+
     string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}" IS_TOP_LEVEL)
 endmacro()
