8f3ea6f201c2e65211d20be600e82bd252f59279
Alexander Bigerl <alexander@bigerl.eu>
2022-08-09 16:41:44 +0200

    enriched example with some assertions and renamed file

--- examples/namespaces.cpp
+++ examples/namespaces.cpp
@@ -7,22 +7,26 @@ int main() {
 
     Namespace ex("http://example.com/");
 
-    auto ex_A = ex + "A";
+    IRI ex_A = ex + "A";
     std::cout << ex_A << std::endl;
+    assert(ex_A.identifier() == "http://example.com/A");
+
     ex.clear();
     ex_A = ex + "A";
+    std::cout << ex_A << std::endl;
+    assert(ex_A.identifier() == "http://example.com/A");
 
     auto rdf = namespaces::RDF();
     auto rdf_Property = rdf + "Property";
     std::cout << rdf_Property << std::endl;
+    assert(rdf_Property.identifier() == "http://www.w3.org/1999/02/22-rdf-syntax-ns#Property");
 
     auto rdf_15 = rdf + "_15";
     std::cout << rdf_15 << std::endl;
+    assert(rdf_15.identifier() == "http://www.w3.org/1999/02/22-rdf-syntax-ns#_15");
     try {
         [[maybe_unused]] auto random = rdf + "random";
     } catch (std::runtime_error const &ex) {
         std::cerr << ex.what() << std::endl;
     }
-
-    auto rdfs = namespaces::RDFS();
 }
