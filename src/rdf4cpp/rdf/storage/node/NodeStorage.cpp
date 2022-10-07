075ae0a1134a561b6252c5e087f5567df841ec09
Alexander Bigerl <alexander@bigerl.eu>
2022-10-07 10:15:20 +0200

    NodeStorage move assignment

--- src/rdf4cpp/rdf/storage/node/NodeStorage.cpp
+++ src/rdf4cpp/rdf/storage/node/NodeStorage.cpp
@@ -88,18 +88,9 @@ NodeStorage &NodeStorage::operator=(const NodeStorage &other) noexcept {
 }
 NodeStorage &NodeStorage::operator=(NodeStorage &&other) noexcept {
     if (this != &other) {
-        if (this->backend_ != other.backend_) {
-            if (this->backend_ != nullptr)
-                this->backend_->dec_use_count();
-            this->backend_ = other.backend_;
-            if (this->backend_ != nullptr)
-                this->backend_->inc_use_count();
-        } else {
-            if (other.backend_ != nullptr) {
-                other.backend_->dec_use_count();
-            }
-        }
-
+        if (this->backend_ != nullptr and this->backend_ != other.backend_)
+            this->backend_->dec_use_count();
+        this->backend_ = other.backend_;
         other.backend_ = nullptr;
     }
     return *this;
