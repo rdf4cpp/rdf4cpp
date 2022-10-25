4d01eb788fbe6ee7ddfa3e544508e58d6236a329
Liss Heidrich <Clueliss@users.noreply.github.com>
2022-10-25 11:07:07 +0200

    added tests for float, double and serializing now standard compliant

--- src/rdf4cpp/rdf/datatypes/registry/util/CharConvExt.hpp
+++ src/rdf4cpp/rdf/datatypes/registry/util/CharConvExt.hpp
@@ -1,12 +1,23 @@
 #ifndef RDF4CPP_REGISTRY_CHARCONVEXT_HPP
 #define RDF4CPP_REGISTRY_CHARCONVEXT_HPP
 
+#include <algorithm>
+#include <cassert>
 #include <charconv>
+#include <cmath>
 #include <concepts>
 #include <stdexcept>
 
 namespace rdf4cpp::rdf::datatypes::registry::util {
 
+/**
+ * Parses a valid string representation of a integral number
+ *
+ * @tparam I the resulting integral type
+ * @param s the string to be parsed
+ * @return the resulting value
+ * @throws std::runtime_error if the string cannot be parsed
+ */
 template<std::integral I>
 I from_chars(std::string_view s) {
     if (s.starts_with('+')) {
@@ -24,6 +35,12 @@ I from_chars(std::string_view s) {
     }
 }
 
+/**
+ * Serializes an integral type into its (SPARQL) canonical representation
+ * see https://www.w3.org/TR/2004/REC-xmlschema-2-20041028/#dt-integer
+ *
+ * @param value the value to be serialized
+ */
 template<std::integral I>
 std::string to_chars(I const value) noexcept {
     // +1 because of definition of digits10 https://en.cppreference.com/w/cpp/types/numeric_limits/digits10
@@ -33,14 +50,19 @@ std::string to_chars(I const value) noexcept {
     std::array<char, buf_sz> buf;
     std::to_chars_result const res = std::to_chars(buf.data(), buf.data() + buf.size(), value);
 
-    if (res.ec != std::errc{}) {
-        assert(false);
-        __builtin_unreachable();
-    }
+    assert(res.ec == std::errc{});
 
     return std::string{buf.data(), res.ptr};
 }
 
+/**
+ * Parses a valid string representation of a floating point number
+ *
+ * @tparam F the result floating point type
+ * @param s the string to be parsed
+ * @return the resulting value
+ * @throws std::runtime_error if the string cannot be parsed
+ */
 template<std::floating_point F>
 F from_chars(std::string_view s) {
     if (s.starts_with('+')) {
@@ -59,20 +81,84 @@ F from_chars(std::string_view s) {
     return value;
 }
 
+namespace detail  {
+/**
+ * equivalent to static_cast<size_t>(1 + std::log10(value))
+ * only exists because the above is not a constexpr in clang
+ */
+template<typename T>
+constexpr size_t log10ceil(T const value) noexcept {
+    if (value < 10) {
+        return 1;
+    }
+    return 1 + log10ceil(value / 10);
+}
+} // namespace detail
+
+/**
+ * Serializes a floating point number into its (SPARQL) canonical string representation
+ * see https://www.w3.org/TR/2004/REC-xmlschema-2-20041028/#dt-float
+ *
+ * @param value the value to be serialized
+ */
 template<std::floating_point F>
 std::string to_chars(F const value) noexcept {
-    // +2 for sign and dot
-    static constexpr size_t buf_sz = 2 + std::numeric_limits<F>::max_exponent10 + std::numeric_limits<F>::max_digits10;
-
-    std::array<char, buf_sz> buf;
-    std::to_chars_result const res = std::to_chars(buf.data(), buf.data() + buf.size(), value, std::chars_format::fixed);
-
-    if (res.ec != std::errc{}) {
-        assert(false);
-        __builtin_unreachable();
+    if (std::isnan(value)) {
+        return "NaN";
     }
 
-    return std::string{buf.data(), res.ptr};
+    if (std::isinf(value)) {
+        if (value > 0) {
+            return "INF";
+        } else {
+            return "-INF";
+        }
+    }
+
+    // +1 for minus in mantissa
+    // +1 for integral part
+    // +1 for dot
+    // +1 for E
+    // +1 for minus in exponent
+    // at least 2 for exponent because the (c++) standard says so (https://en.cppreference.com/w/cpp/utility/to_chars)
+    static constexpr size_t buf_sz = 5 + std::numeric_limits<F>::max_digits10 + std::max(2ul, detail::log10ceil(std::numeric_limits<F>::max_exponent10));
+    std::array<char, buf_sz> buf;
+
+    std::to_chars_result res = [&]() {
+        [[maybe_unused]] F integral;
+        F const fraction = std::modf(value, &integral);
+
+        if (fraction == 0) {
+            // include one zero after dot
+            return std::to_chars(buf.data(), buf.data() + buf.size(), value, std::chars_format::scientific, 1);
+        } else {
+            // automatic precision guarantees round-trip-safety (https://en.cppreference.com/w/cpp/utility/to_chars)
+            return std::to_chars(buf.data(), buf.data() + buf.size(), value, std::chars_format::scientific);
+        }
+    }();
+    assert(res.ec == std::errc{});
+
+    auto *e_ptr = std::find(buf.data(), res.ptr, 'e');
+    assert(e_ptr != res.ptr); // serializing in scientific notation, there must be an 'e'
+    *e_ptr = 'E'; // convert 'e' to 'E' as required by the SPARQL standard
+
+    auto const shift_amt = [&]() {
+        auto *exp_start_ptr = e_ptr + 2;
+        if (auto *non_zero_exp_ptr = std::find_if(exp_start_ptr, res.ptr, [](char const c) { return c != '0'; }); non_zero_exp_ptr != res.ptr) {
+            // exponent has leading zeros
+            // shift such that leading zeros get shifted out
+            return non_zero_exp_ptr - exp_start_ptr;
+        } else {
+            // exponent is only zeros
+            // shift such that one zero remains
+            return res.ptr - exp_start_ptr - 1;
+        }
+    }();
+
+    auto const shift_off = static_cast<std::string::size_type>(*(e_ptr + 1) == '+'); // needed to shift potential '+' out as well
+
+    auto *new_end = std::shift_left(e_ptr + 1 + (1 - shift_off), res.ptr, shift_amt + shift_off); // shift out all leading zeros and plus sign from exponent
+    return std::string{buf.data(), static_cast<std::string::size_type>(new_end - buf.data())};
 }
 
 } // namespace rdf4cpp::rdf::datatypes::registry::util
---
+++ tests/datatype/tests_Double.cpp
@@ -0,0 +1,90 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest/doctest.h>
+#include <rdf4cpp/rdf.hpp>
+#include <cmath>
+
+using namespace rdf4cpp::rdf;
+
+TEST_CASE("Datatype Double") {
+    using type = datatypes::xsd::Double::cpp_type;
+
+    constexpr auto correct_type_iri_cstr = "http://www.w3.org/2001/XMLSchema#double";
+    CHECK(std::string(datatypes::xsd::Double::identifier) == correct_type_iri_cstr);
+    auto type_iri = IRI(datatypes::xsd::Double::identifier);
+    CHECK(type_iri.identifier() == correct_type_iri_cstr);
+
+    std::string rdf_dbl_1_0{"1.0E0"};
+
+    double value = 1.00;
+    auto lit1 = Literal::make<datatypes::xsd::Double>(value);
+    CHECK(lit1.value<datatypes::xsd::Double>() == value);
+    CHECK(lit1.lexical_form() == rdf_dbl_1_0);
+
+    value = 987456321123456.123586987;
+    auto lit2 = Literal::make<datatypes::xsd::Double>(value);
+    CHECK(lit2.value<datatypes::xsd::Double>() == value);
+
+    value = -64545352389.2352345670;
+    auto lit3 = Literal::make<datatypes::xsd::Double>(value);
+    CHECK(lit3.value<datatypes::xsd::Double>() == value);
+
+    value = 1;
+    auto lit4 = Literal::make<datatypes::xsd::Double>(value);
+    CHECK(lit4.value<datatypes::xsd::Double>() == value);
+    CHECK(lit4.lexical_form() == rdf_dbl_1_0);
+
+    value = 1;
+    auto lit6 = Literal{std::to_string(value), type_iri};
+    CHECK(lit6.value<datatypes::xsd::Double>() == value);
+    CHECK(lit6.lexical_form() == rdf_dbl_1_0);
+
+    value = 987456321123456.123586987;
+    auto lit7 = Literal{std::to_string(value), type_iri};
+    CHECK(lit7.value<datatypes::xsd::Double>() == value);
+
+    auto lit8 = Literal{"NaN", type_iri};
+    CHECK(std::isnan(lit8.value<datatypes::xsd::Double>()));
+
+    auto lit9 = Literal{"INF", type_iri};
+    CHECK(std::isinf(lit9.value<datatypes::xsd::Double>()));
+
+    value = INFINITY;
+    auto lit10 = Literal::make<datatypes::xsd::Double>(value);
+    CHECK(std::isinf(lit10.value<datatypes::xsd::Double>()));
+
+    value = NAN;
+    auto lit11 = Literal::make<datatypes::xsd::Double>(value);
+    CHECK(std::isnan(lit11.value<datatypes::xsd::Double>()));
+
+    value = 2.22e-308;
+    auto lit12 = Literal{"2.22e-308", type_iri};
+    CHECK(lit12.value<datatypes::xsd::Double>() == value);
+
+    auto lit13 = Literal{"+INF", type_iri};
+    CHECK(std::isinf(lit13.value<datatypes::xsd::Double>()));
+
+    auto lit14 = Literal{"-INF", type_iri};
+    CHECK(std::isinf(lit14.value<datatypes::xsd::Double>()));
+    CHECK(lit14.value<datatypes::xsd::Double>() == -std::numeric_limits<type>::infinity());
+
+    value = -INFINITY;
+    auto lit15 = Literal::make<datatypes::xsd::Double>(value);
+    CHECK(std::isinf(lit15.value<datatypes::xsd::Double>()));
+    CHECK(lit15.value<datatypes::xsd::Double>() == -std::numeric_limits<type>::infinity());
+
+    CHECK(lit1 != lit2);
+    CHECK(lit2 != lit3);
+    CHECK(lit1 == lit4);
+    CHECK(lit1 == lit6);
+    CHECK(lit2 == lit7);
+    CHECK(lit9 == lit10);
+    CHECK(lit9 == lit13);
+    CHECK(lit10 == lit13);
+    CHECK(lit14 == lit15);
+
+    // suppress warnings regarding attribute ‘nodiscard’
+    Literal no_discard_dummy;
+
+    CHECK_THROWS(no_discard_dummy = Literal("454sdsd", type_iri));
+}
