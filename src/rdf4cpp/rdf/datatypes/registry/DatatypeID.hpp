b1129d7f23fefdf3a83103dcbeaaa32c6bedef4d
Liss Heidrich <Clueliss@users.noreply.github.com>
2022-11-09 23:00:12 +0100

    constexpr for DatatypeIDVisitor

--- src/rdf4cpp/rdf/datatypes/registry/DatatypeID.hpp
+++ src/rdf4cpp/rdf/datatypes/registry/DatatypeID.hpp
@@ -39,12 +39,12 @@ struct DatatypeIDVisitor {
     F map_fixed;
     D map_dynamic;
 
-    auto operator()(storage::node::identifier::LiteralType const fixed) noexcept(std::is_nothrow_invocable_v<F, storage::node::identifier::LiteralType const>) -> std::invoke_result_t<F, storage::node::identifier::LiteralType> {
+    constexpr auto operator()(storage::node::identifier::LiteralType const fixed) noexcept(std::is_nothrow_invocable_v<F, storage::node::identifier::LiteralType const>) -> std::invoke_result_t<F, storage::node::identifier::LiteralType> {
         return std::invoke(this->map_fixed, fixed);
     }
 
     template<typename S>
-    auto operator()(S &&other) noexcept(std::is_nothrow_invocable_v<S, decltype(std::forward<S>(other))>) -> std::invoke_result_t<D, decltype(std::forward<S>(other))> {
+    constexpr auto operator()(S &&other) noexcept(std::is_nothrow_invocable_v<S, decltype(std::forward<S>(other))>) -> std::invoke_result_t<D, decltype(std::forward<S>(other))> {
         return std::invoke(this->map_dynamic, std::forward<S>(other));
     }
 };
