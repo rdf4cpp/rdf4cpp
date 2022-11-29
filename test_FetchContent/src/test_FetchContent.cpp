b161cce372b213e439e287a2fb6f76f9b3160017
Liss Heidrich <Clueliss@users.noreply.github.com>
2022-11-29 11:27:45 +0100

    more robust packaging tests

--- test_FetchContent/src/test_FetchContent.cpp
+++ test_FetchContent/src/test_FetchContent.cpp
@@ -3,5 +3,9 @@
 #include <iostream>
 
 int main() {
-	std::cout << rdf4cpp::rdf::Literal("FetchContent works fine.") << std::endl;
+    using namespace rdf4cpp::rdf;
+
+    Literal datatype_test_lit{"123.0", IRI{"http://www.w3.org/2001/XMLSchema#double"}}; // segfault if registering of datatypes does not work
+    std::cout << datatype_test_lit << std::endl;
+    std::cout << Literal{"Using FetchContent works."} << std::endl;
 }
--- test_package/example.cpp
+++ test_package/example.cpp
@@ -5,6 +5,7 @@
 int main() {
     using namespace rdf4cpp::rdf;
 
-    [[maybe_unused]] Literal datatype_test_lit{"123.0", IRI{"http://www.w3.org/2001/XMLSchema#double"}}; // segfault if registering of datatypes does not work
+    Literal datatype_test_lit{"123.0", IRI{"http://www.w3.org/2001/XMLSchema#double"}}; // segfault if registering of datatypes does not work
+    std::cout << datatype_test_lit << std::endl;
     std::cout << Literal{"Using the conan package works."} << std::endl;
 }
