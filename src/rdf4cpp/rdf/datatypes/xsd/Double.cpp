d29b700a1d10e9c4efc2facd7ee85aee6d016e7a
Liss Heidrich <Clueliss@users.noreply.github.com>
2022-12-19 17:00:09 +0100

    impl double inlining by dropping part of mantissa

--- src/rdf4cpp/rdf/datatypes/xsd/Double.cpp
+++ src/rdf4cpp/rdf/datatypes/xsd/Double.cpp
@@ -20,10 +20,30 @@ bool capabilities::Logical<xsd_double>::effective_boolean_value(cpp_type const &
     return !std::isnan(value) && value != 0.0;
 }
 
+template<>
+std::optional<uint64_t> capabilities::Inlineable<xsd_double>::try_into_inlined(cpp_type const &value) noexcept {
+    static constexpr uint64_t drop_width = 64 - storage::node::identifier::LiteralID::width;
+
+    auto const packed = util::pack<uint64_t>(value);
+    auto const shortened = packed >> drop_width;
+
+    if (shortened << drop_width != packed) {
+        return std::nullopt;
+    }
+
+    return shortened;
+}
+
+template<>
+capabilities::Inlineable<xsd_double>::cpp_type capabilities::Inlineable<xsd_double>::from_inlined(uint64_t inlined) noexcept {
+    return util::unpack<double>(inlined << (64 - storage::node::identifier::LiteralID::width));
+}
+
 template struct LiteralDatatypeImpl<xsd_double,
                                     capabilities::Logical,
                                     capabilities::Numeric,
                                     capabilities::Comparable,
-                                    capabilities::FixedId>;
+                                    capabilities::FixedId,
+                                    capabilities::Inlineable>;
 
 }  // namespace rdf4cpp::rdf::datatypes::registry
