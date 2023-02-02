ac36ccb04d40cb8a061fce7696c6af75b19eda91
Alexander Bigerl <alexander@bigerl.eu>
2023-02-02 17:52:47 +0100

    add private to conan

--- conanfile.py
+++ conanfile.py
@@ -22,7 +22,7 @@ class Recipe(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
     exports = "LICENSE",
-    exports_sources = "src/*", "CMakeLists.txt", "cmake/*"
+    exports_sources = "src/*", "private/*", "CMakeLists.txt", "cmake/*"
     requires = (("fmt/9.0.0", "private"),  # format must only be used within cpp files
                 "expected-lite/0.6.2",
                 "boost/1.79.0",
