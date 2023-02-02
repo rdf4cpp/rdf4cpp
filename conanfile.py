8fe529059d0bf24ce1bc6c4aac9609c76de09a70
Alexander Bigerl <alexander@bigerl.eu>
2023-02-02 17:59:19 +0100

    fix missing dependency re2 when included via conan

--- conanfile.py
+++ conanfile.py
@@ -26,7 +26,7 @@ class Recipe(ConanFile):
     requires = (("fmt/9.0.0", "private"),  # format must only be used within cpp files
                 "expected-lite/0.6.2",
                 "boost/1.79.0",
-                ("re2/20221201", "private"),
+                "re2/20221201",
                 ("utfcpp/3.2.3", "private"))
 
     generators = ("CMakeDeps", "CMakeToolchain")
