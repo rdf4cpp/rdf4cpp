3e65401aafde17458d5e2ed671f802b5c28e0ea0
mcb <mgrbo@web.de>
2023-02-08 16:02:30 +0100

    first implementation of RDFFileParser, needs testing

---
+++ src/rdf4cpp/rdf/parser/RDFFileParser.cpp
@@ -0,0 +1,47 @@
+#include "RDFFileParser.hpp"
+
+rdf4cpp::rdf::parser::RDFFileParser::RDFFileParser(const std::string &filePath, rdf4cpp::rdf::parser::ParsingFlags flags,
+                                                   rdf4cpp::rdf::storage::node::NodeStorage node_storage)
+    : filePath_(filePath), flags_(std::move(flags)), node_storage_(std::move(node_storage)) {
+}
+rdf4cpp::rdf::parser::RDFFileParser::RDFFileParser(std::string &&filePath, rdf4cpp::rdf::parser::ParsingFlags flags,
+                                                   rdf4cpp::rdf::storage::node::NodeStorage node_storage)
+    : filePath_(std::move(filePath)), flags_(std::move(flags)), node_storage_(std::move(node_storage)) {
+}
+rdf4cpp::rdf::parser::RDFFileParser::Iterator rdf4cpp::rdf::parser::RDFFileParser::begin() {
+    std::ifstream stream{filePath_};
+    if (!stream.is_open())
+        return {nullptr, nullptr};
+    return {std::make_unique<std::ifstream>(std::move(stream)),
+            std::make_unique<IStreamQuadIterator>(stream, flags_, IStreamQuadIterator::prefix_storage_type {}, node_storage_)};
+}
+std::default_sentinel_t rdf4cpp::rdf::parser::RDFFileParser::end() {
+    return {};
+}
+
+rdf4cpp::rdf::parser::RDFFileParser::Iterator::Iterator(std::unique_ptr<std::ifstream> &&stream, std::unique_ptr<IStreamQuadIterator> &&iter)
+    : stream_(std::move(stream)), iter_(std::move(iter)) {
+}
+rdf4cpp::rdf::parser::RDFFileParser::Iterator::reference rdf4cpp::rdf::parser::RDFFileParser::Iterator::operator*() const noexcept {
+    return (*iter_).operator*();
+}
+rdf4cpp::rdf::parser::RDFFileParser::Iterator::pointer rdf4cpp::rdf::parser::RDFFileParser::Iterator::operator->() const noexcept {
+    return (*iter_).operator->();
+}
+rdf4cpp::rdf::parser::RDFFileParser::Iterator &rdf4cpp::rdf::parser::RDFFileParser::Iterator::operator++() {
+    ++(*iter_);
+    return *this;
+}
+bool rdf4cpp::rdf::parser::RDFFileParser::Iterator::operator==(const rdf4cpp::rdf::parser::RDFFileParser::Iterator &other) const noexcept {
+    if (iter_ == nullptr || other.iter_ == nullptr)
+        return iter_ == other.iter_;
+    return (*iter_)==(*other.iter_);
+}
+bool rdf4cpp::rdf::parser::operator==(const rdf4cpp::rdf::parser::RDFFileParser::Iterator &iter, std::default_sentinel_t s) noexcept {
+    if (iter.iter_ == nullptr)
+        return true;
+    return (*iter.iter_) == s;
+}
+bool rdf4cpp::rdf::parser::operator==(std::default_sentinel_t s, const rdf4cpp::rdf::parser::RDFFileParser::Iterator& iter) noexcept {
+    return iter == s;
+}
