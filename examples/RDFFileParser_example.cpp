e2244960c23fd148b2b954aa39f95426939e37f9
mcb <mgrbo@web.de>
2023-02-15 13:48:58 +0100

    formatted example

--- examples/RDFFileParser_example.cpp
+++ examples/RDFFileParser_example.cpp
@@ -3,10 +3,10 @@
 
 int main() {
     // read file
-    for (const auto& v : rdf4cpp::rdf::parser::RDFFileParser{"./RDFFileParser_simple.ttl"}) {
-        if (v.has_value()) // check if parser returns a successful read value
+    for (const auto &v : rdf4cpp::rdf::parser::RDFFileParser{"./RDFFileParser_simple.ttl"}) {
+        if (v.has_value())  // check if parser returns a successful read value
             std::cout << v.value() << "\n";
-        else // or an error
+        else  // or an error
             std::cout << v.error() << "\n";
     }
     return 0;
