82e5a445ecd8c5d7468dd7e4c38d6f29b2a4455c
mcb <mgrbo@web.de>
2023-02-16 11:34:08 +0100

    fixing minor issues from PR

--- src/rdf4cpp/rdf/parser/RDFFileParser.cpp
+++ src/rdf4cpp/rdf/parser/RDFFileParser.cpp
@@ -1,5 +1,6 @@
-#include "RDFFileParser.hpp"
+#include <rdf4cpp/rdf/parser/RDFFileParser.hpp>
 
+namespace rdf4cpp::rdf::parser {
 rdf4cpp::rdf::parser::RDFFileParser::RDFFileParser(const std::string &file_path, rdf4cpp::rdf::parser::ParsingFlags flags,
                                                    rdf4cpp::rdf::storage::node::NodeStorage node_storage)
     : file_path_(file_path), flags_(std::move(flags)), node_storage_(std::move(node_storage)) {
@@ -8,13 +9,13 @@ rdf4cpp::rdf::parser::RDFFileParser::RDFFileParser(std::string &&file_path, rdf4
                                                    rdf4cpp::rdf::storage::node::NodeStorage node_storage)
     : file_path_(std::move(file_path)), flags_(std::move(flags)), node_storage_(std::move(node_storage)) {
 }
-rdf4cpp::rdf::parser::RDFFileParser::Iterator rdf4cpp::rdf::parser::RDFFileParser::begin() {
+rdf4cpp::rdf::parser::RDFFileParser::Iterator rdf4cpp::rdf::parser::RDFFileParser::begin() const {
     std::ifstream stream{file_path_};
     if (!stream.is_open())
         return {std::move(stream), {}};
     return {std::move(stream), flags_, node_storage_};
 }
-std::default_sentinel_t rdf4cpp::rdf::parser::RDFFileParser::end() {
+std::default_sentinel_t rdf4cpp::rdf::parser::RDFFileParser::end() const noexcept {
     return {};
 }
 
@@ -38,11 +39,12 @@ rdf4cpp::rdf::parser::RDFFileParser::Iterator &rdf4cpp::rdf::parser::RDFFilePars
 bool rdf4cpp::rdf::parser::RDFFileParser::Iterator::operator==(const rdf4cpp::rdf::parser::RDFFileParser::Iterator &other) const noexcept {
     return iter_ == other.iter_;
 }
-bool rdf4cpp::rdf::parser::operator==(const rdf4cpp::rdf::parser::RDFFileParser::Iterator &iter, std::default_sentinel_t s) noexcept {
+bool operator==(const rdf4cpp::rdf::parser::RDFFileParser::Iterator &iter, std::default_sentinel_t s) noexcept {
     if (!iter.stream_.is_open())
         return true;
     return iter.iter_ == s;
 }
-bool rdf4cpp::rdf::parser::operator==(std::default_sentinel_t s, const rdf4cpp::rdf::parser::RDFFileParser::Iterator &iter) noexcept {
+bool operator==(std::default_sentinel_t s, const rdf4cpp::rdf::parser::RDFFileParser::Iterator &iter) noexcept {
     return iter == s;
 }
+}  // namespace rdf4cpp::rdf::parser
