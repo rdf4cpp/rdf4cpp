d39f57633e4ea68473a8ebaff539156f7ea46fb8
mcb <mgrbo@web.de>
2023-04-19 14:25:45 +0200

    tostring fixes, multiplication, test cases cleanup

--- src/rdf4cpp/rdf/util/BigDecimal.hpp
+++ src/rdf4cpp/rdf/util/BigDecimal.hpp
@@ -14,12 +14,37 @@ class BigDecimal {
     Exponent_t exponent;
     bool sign;
 
+    static_assert(!std::is_integral_v<boost::multiprecision::cpp_int>);
+
     static constexpr uint32_t base = 10;
 
     static constexpr UnscaledValue_t shift_pow(UnscaledValue_t v, Exponent_t ex) {
         for (Exponent_t i = 0; i < ex; ++i)
             v *= base;
         return v;
+    }
+
+    template<class T>
+    static constexpr auto save_add(const T &a, const T &b, const char *exc) {
+        if constexpr (std::is_integral_v<T>) {
+            T result;
+            if (__builtin_add_overflow(a, b, &result))
+                throw std::overflow_error{exc};
+            return result;
+        } else {
+            return a + b;
+        }
+    }
+    template<class T>
+    static constexpr auto save_mul(const T &a, const T &b, const char *exc) {
+        if constexpr (std::is_integral_v<T>) {
+            T result;
+            if (__builtin_mul_overflow(a, b, &result))
+                throw std::overflow_error{exc};
+            return result;
+        } else {
+            return a * b;
+        }
     }
 
 public:
@@ -37,31 +62,41 @@ public:
         return exponent;
     }
 
-    constexpr std::strong_ordering operator<=>(const BigDecimal& other) const noexcept {
-            if (this->sign != other.sign)
-                return this->sign == true ? std::strong_ordering::less : std::strong_ordering::greater;
-            UnscaledValue_t t = this->unscaled_value;
-            UnscaledValue_t o = other.unscaled_value;
-            if (this->exponent > other.exponent) {
-                o = shift_pow(o, this->exponent - other.exponent);
-            }
-            else if (this->exponent < other.exponent) {
-                t = shift_pow(t, other.exponent - this->exponent);
-            }
-            if (t < o)
-                return std::strong_ordering::less;
-            else if(t > o)
-                return std::strong_ordering::greater;
-            else
-                return std::strong_ordering::equivalent;
+    constexpr BigDecimal operator*(const BigDecimal &other) const {
+        return BigDecimal{save_mul(this->unscaled_value, other.unscaled_value, "BigDecimal::operator* unscaled overflow"),
+                          save_add(this->exponent, other.exponent, "BigDecimal::operator* exponent overflow")};
+    }
+
+    constexpr std::strong_ordering operator<=>(const BigDecimal &other) const noexcept {
+        if (this->sign != other.sign)
+            return this->sign == true ? std::strong_ordering::less : std::strong_ordering::greater;
+        UnscaledValue_t t = this->unscaled_value;
+        UnscaledValue_t o = other.unscaled_value;
+        if (this->exponent > other.exponent) {
+            o = shift_pow(o, this->exponent - other.exponent);
+        } else if (this->exponent < other.exponent) {
+            t = shift_pow(t, other.exponent - this->exponent);
+        }
+        if (t < o)
+            return std::strong_ordering::less;
+        else if (t > o)
+            return std::strong_ordering::greater;
+        else
+            return std::strong_ordering::equivalent;
     };
-    constexpr bool operator==(const BigDecimal& other) const noexcept {
-            return *this <=> other == std::strong_ordering::equivalent;
+    constexpr bool operator==(const BigDecimal &other) const noexcept {
+        return *this <=> other == std::strong_ordering::equivalent;
     }
 
     constexpr explicit operator double() const noexcept {
         double v = static_cast<double>(unscaled_value) * std::pow(static_cast<double>(base), -static_cast<double>(exponent));
         return sign ? -v : v;
+    }
+
+    friend std::ostream &operator<<(std::ostream &str, const BigDecimal &bn) {
+        auto s = static_cast<std::string>(bn);
+        str << s;
+        return str;
     }
 
     explicit operator std::string() const noexcept {
@@ -70,23 +105,29 @@ public:
         Exponent_t ex = exponent;
         bool hasDot = false;
         while (v != 0) {
-                if (!hasDot && ex == 0) {
-                    s << '.';
-                    hasDot = true;
-                }
-                else {
-                    --ex;
-                }
-                uint32_t c = static_cast<uint32_t>(v % base);
-                s << c;
-                v /= base;
+            if (!hasDot && ex == 0) {
+                if (s.view().size() == 0)
+                    s << '0';
+                s << '.';
+                hasDot = true;
+            } else {
+                --ex;
+            }
+            auto c = static_cast<uint32_t>(v % base);
+            s << c;
+            v /= base;
+        }
+        if (!hasDot) {
+            for (Exponent_t i = 0; i < ex; ++i)
+                s << '0';
+            s << ".0";
         }
         if (sign)
-                s << '-';
+            s << '-';
         std::string_view sv = s.view();
-        return std::string {sv.rbegin(), sv.rend()};
+        return std::string{sv.rbegin(), sv.rend()};
     }
 };
-}
+}  // namespace rdf4cpp::rdf::util
 
 #endif  //RDF4CPP_BIGDECIMAL_H
--- tests/util/tests_BigDecimal.cpp
+++ tests/util/tests_BigDecimal.cpp
@@ -6,23 +6,55 @@
 using Dec = rdf4cpp::rdf::util::BigDecimal<>;
 
 TEST_CASE("basics") {
-    Dec d{500, 1};
-    CHECK(static_cast<double>(d) == 50.0);
-    CHECK(d > Dec{500, 1, true});
-    CHECK(Dec{500, 1, true} < d);
-    CHECK(Dec{50,0} == d);
-    CHECK(d > Dec{40,0});
-    CHECK(d < Dec{60,0});
-    CHECK(Dec{40,0} < d);
-    CHECK(Dec{60,0} > d);
-    CHECK(d != Dec{60,1});
-    d.normalize();
-    CHECK(static_cast<double>(d) == 50.0);
-    CHECK(d.get_exponent() == 0);
-    d = Dec{51,1};
-    d.normalize();
-    CHECK(d == Dec{51,1});
-    CHECK(d.get_exponent() == 1);
-    CHECK(static_cast<std::string>(d) == "5.1");
-    CHECK(static_cast<std::string>(Dec{9514,2,true}) == "-95.14");
+    SUBCASE("ctor and compare") {
+        Dec d{500, 1};
+        CHECK(d > Dec{500, 1, true});
+        CHECK(Dec{500, 1, true} < d);
+        CHECK(Dec{50, 0} == d);
+        CHECK(d > Dec{40, 0});
+        CHECK(d < Dec{60, 0});
+        CHECK(Dec{40, 0} < d);
+        CHECK(Dec{60, 0} > d);
+        CHECK(d != Dec{60, 1});
+    }
+    SUBCASE("normalize") {
+        Dec d{500, 1};
+        d.normalize();
+        CHECK(d == Dec{50, 0});
+        CHECK(d.get_exponent() == 0);
+        d = Dec{51, 1};
+        d.normalize();
+        CHECK(d == Dec{51, 1});
+        CHECK(d.get_exponent() == 1);
+    }
+}
+
+TEST_CASE("arithmetic") {
+    SUBCASE("*") {
+        CHECK((Dec{2, 0} * Dec{2, 0}) == Dec{4, 0});
+        CHECK((Dec{20, 0} * Dec{20, 0}) == Dec{400, 0});
+        CHECK((Dec{2, 1} * Dec{2, 0}) == Dec{4, 1});
+        CHECK((Dec{2, 1} * Dec{2, 1}) == Dec{4, 2});
+        CHECK_THROWS_AS((Dec{2, std::numeric_limits<uint32_t>::max()} * Dec{2, std::numeric_limits<uint32_t>::max()}), std::overflow_error);
+    }
+}
+
+TEST_CASE("conversion") {
+    SUBCASE("std::string") {
+        CHECK(static_cast<std::string>(Dec{51, 1}) == "5.1");
+        CHECK(static_cast<std::string>(Dec{9514, 2, true}) == "-95.14");
+        CHECK(static_cast<std::string>(Dec{9514, 0, true}) == "-9514.0");
+        CHECK(static_cast<std::string>(Dec{9514, 4, true}) == "-0.9514");
+        CHECK(static_cast<std::string>(Dec{9514, 6, true}) == "-0.009514");
+    }
+    SUBCASE("writing") {
+        std::stringstream str{};
+        str << Dec{50, 1};
+        CHECK(str.view() == "5.0");
+        // uses string conversion, so no more tests here
+    }
+    SUBCASE("double") {
+        CHECK(static_cast<double>(Dec{50, 0}) == 50.0);
+        CHECK(static_cast<double>(Dec{500, 1}) == 50.0);
+    }
 }
