60505737b26accdbc05f16d4ddc2aadfb3c5b4c8
mcb <mgrbo@web.de>
2023-06-14 13:23:47 +0200

    fixed compile

--- src/rdf4cpp/rdf/datatypes/xsd/Decimal.cpp
+++ src/rdf4cpp/rdf/datatypes/xsd/Decimal.cpp
@@ -33,7 +33,7 @@ std::string capabilities::Default<xsd_decimal>::to_simplified_string(cpp_type co
     cpp_type v = value;
     v.normalize();
     if (v.get_exponent() == 0) {
-        return boost::multiprecision::to_string(static_cast<boost::multiprecision::cpp_int>(v));
+        return static_cast<boost::multiprecision::cpp_int>(v).str();
     } else {
         return static_cast<std::string>(v);
     }
