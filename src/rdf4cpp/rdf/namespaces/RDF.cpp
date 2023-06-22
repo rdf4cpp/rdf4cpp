bf240433cb38eb4db83f57ae09c7945aeaa74fac
Liss Heidrich <Clueliss@users.noreply.github.com>
2023-06-22 10:49:01 +0200

    revert change and add explaining comment

--- src/rdf4cpp/rdf/namespaces/RDF.cpp
+++ src/rdf4cpp/rdf/namespaces/RDF.cpp
@@ -11,7 +11,7 @@ IRI RDF::operator+(std::string_view suffix) const {
         auto from_chars_result = std::from_chars(suffix.data() + 1, suffix.data() + suffix.size(), id);
         if (from_chars_result.ec == std::errc() and from_chars_result.ptr == suffix.data() + suffix.size()) {
             std::string normalized_suffix = std::string{"_"} + std::to_string(id);
-            return ClosedNamespace::operator+(normalized_suffix);
+            return Namespace::operator+(normalized_suffix); // add rdf:_<id> which is also a valid member of this namespace
         } else {
             std::ostringstream oss;
             oss << "Resource " << suffix << " does not exist within the vocabulary " << namespace_iri_ << '.';
