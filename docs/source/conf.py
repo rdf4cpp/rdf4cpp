41895a189856f04e459b480bc61cdfcb3d954a43
mcb <mgrbo@web.de>
2023-09-06 16:46:47 +0200

    sphinx basic setup

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import subprocess, os
+subprocess.call('cd ../../; doxygen', shell=True)
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'rdf4cpp'
+copyright = '2023, DICE Group'
+author = 'DICE Group'
+release = '0.0.14'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["breathe"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+breathe_projects = {
+    "rdf4cpp": "../../doc/xml/",
+}
+breathe_default_project = "rdf4cpp"
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. rdf4cpp documentation master file, created by
+   sphinx-quickstart on Wed Sep  6 14:52:38 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to rdf4cpp's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+.. doxygenclass:: rdf4cpp::rdf::Literal
+    :members:
+
+* :ref:`search`
