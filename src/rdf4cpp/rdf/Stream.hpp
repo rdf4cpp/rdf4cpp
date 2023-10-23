8383973ceb9d25e2946e487c4bb907f8cd397c29
Liss Heidrich <Clueliss@users.noreply.github.com>
2023-10-23 14:25:58 +0200

    constexrp

--- src/rdf4cpp/rdf/Stream.hpp
+++ src/rdf4cpp/rdf/Stream.hpp
@@ -14,7 +14,7 @@ struct Sink {
     void (*size_hint)(size_t hint, void *stream);
     int (*error)(void *stream);
 
-    [[nodiscard]] static Sink make_string_sink() noexcept {
+    [[nodiscard]] static constexpr Sink make_string_sink() noexcept {
         return Sink{
                 .write = [](void const *buf, size_t elem_size, size_t len, void *stream) {
                     reinterpret_cast<std::string *>(stream)->append(reinterpret_cast<char const *>(buf), elem_size * len);
@@ -28,7 +28,7 @@ struct Sink {
                 }};
     }
 
-    [[nodiscard]] static Sink make_ostream_sink() noexcept {
+    [[nodiscard]] static constexpr Sink make_ostream_sink() noexcept {
         return Sink{
                 .write = [](void const *buf, size_t elem_size, size_t len, void *stream_) {
                     auto *stream = reinterpret_cast<std::ostream *>(stream_);
@@ -42,7 +42,7 @@ struct Sink {
                 }};
     }
 
-    [[nodiscard]] static Sink make_c_file_sink() noexcept {
+    [[nodiscard]] static constexpr Sink make_c_file_sink() noexcept {
         return Sink{
                 .write = [](void const *buf, size_t elem_size, size_t len, void *stream) {
                     return fwrite(buf, elem_size, len, reinterpret_cast<FILE *>(stream));
@@ -61,7 +61,7 @@ struct Source {
     size_t (*size_hint)(void *stream);
     int (*error)(void *stream);
 
-    [[nodiscard]] static Source make_string_view_source() noexcept {
+    [[nodiscard]] static constexpr Source make_string_view_source() noexcept {
         return Source{
                 .read = [](void *buf, size_t elem_size, size_t len, void *stream_) {
                     auto const max_read = elem_size * len;
@@ -79,7 +79,7 @@ struct Source {
                 }};
     }
 
-    [[nodiscard]] static Source make_istream_source() noexcept {
+    [[nodiscard]] static constexpr Source make_istream_source() noexcept {
         return Source{
                 .read = [](void *buf, size_t elem_size, size_t len, void *stream_) {
                     auto *stream = reinterpret_cast<std::istream *>(stream_);
@@ -94,7 +94,7 @@ struct Source {
                 }};
     }
 
-    [[nodiscard]] static Source make_c_file_source() noexcept {
+    [[nodiscard]] static constexpr Source make_c_file_source() noexcept {
         return Source{
                 .read = [](void *buf, size_t elem_size, size_t len, void *stream) {
                     return fread(buf, elem_size, len, reinterpret_cast<FILE *>(stream));
