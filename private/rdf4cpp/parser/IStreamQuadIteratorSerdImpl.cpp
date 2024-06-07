dd4bc2a1327eb4e8d7835d1bb6a55434edd3acd9
mcb <mgrbo@web.de>
2024-06-07 13:46:11 +0200

    catch InvalidNode

--- private/rdf4cpp/parser/IStreamQuadIteratorSerdImpl.cpp
+++ private/rdf4cpp/parser/IStreamQuadIteratorSerdImpl.cpp
@@ -39,7 +39,7 @@ nonstd::expected<Node, SerdStatus> IStreamQuadIterator::Impl::get_bnode(std::str
     if (this->flags.contains(ParsingFlag::KeepBlankNodeIds)) {
         try {
             return BlankNode{node_str, this->state->node_storage};
-        } catch (std::runtime_error const &e) {
+        } catch (InvalidNode const &e) {
             // TODO: check when actual blank node validation implemented
             // NOTE: line, col not entirely accurate as this function is called after a triple was parsed
             this->last_error = ParsingError{.error_type = ParsingError::Type::BadBlankNode,
@@ -58,7 +58,7 @@ nonstd::expected<Node, SerdStatus> IStreamQuadIterator::Impl::get_bnode(std::str
         return this->state->blank_node_scope_manager->scope(graph_str).get_or_generate_node(node_str,
                                                                                             *this->state->blank_node_generator,
                                                                                             this->state->node_storage);
-    } catch (std::runtime_error const &e) {
+    } catch (InvalidNode const &e) {
         // TODO: check when actual blank node validation implemented
         // NOTE: line, col not entirely accurate as this function is called after a triple was parsed
         this->last_error = ParsingError{.error_type = ParsingError::Type::BadBlankNode,
@@ -169,7 +169,7 @@ nonstd::expected<Literal, SerdStatus> IStreamQuadIterator::Impl::get_literal(Ser
         } else {
             return Literal::make_simple(literal_value, this->state->node_storage);
         }
-    } catch (std::runtime_error const &e) {
+    } catch (InvalidNode const &e) {
         // NOTE: line, col not entirely accurate as this function is called after a triple was parsed
         this->last_error = ParsingError{.error_type = ParsingError::Type::BadLiteral,
                                         .line = serd_reader_get_current_line(this->reader),
