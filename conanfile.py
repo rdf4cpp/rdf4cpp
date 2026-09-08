bb7ee1638320e9ef40e69f51858ad650cc43bc71
Liss Heidrich <31625940+liss-h@users.noreply.github.com>
2026-09-08 17:50:17 +0200

    fix CI

--- conanfile.py
+++ conanfile.py
@@ -30,9 +30,7 @@ class Recipe(ConanFile):
     generators = ("CMakeDeps", "CMakeToolchain")
 
     def requirements(self):
-        # Would like to use >= 1.91 because <=1.90 has a bug in charconv for __int128.
-        # But 1.91 does not compile on aarch64-linux.
-        self.requires("boost/1.90.0", transitive_headers=True)
+        self.requires("boost/1.91.0", transitive_headers=True)  # >= 1.91 because <=1.90 has a bug in charconv for __int128
         self.requires("expected-lite/0.9.0", transitive_headers=True)
         self.requires("pcre2/10.47", options={"support_jit": True})
         self.requires("openssl/3.6.3")
